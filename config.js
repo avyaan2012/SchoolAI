d72f2745d80fa8a4a089f855e94933a460a8b8d0
Avyaan Maheshwari <your.email@example.com>
2026-06-25 20:29:54 +0530

    entered client id in configure.js in repository SchoolAI

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ClassQuiz AI Google Classroom config
 // Paste your OAuth Web Client ID between the quotes below.
 // Example: const GOOGLE_CLIENT_ID = "1234567890-abc.apps.googleusercontent.com";
-const GOOGLE_CLIENT_ID = "PASTE_YOUR_GOOGLE_CLIENT_ID_HERE";
+const GOOGLE_CLIENT_ID = "138041635688-obckndeapof73mat9tqjdv8q4eg2cdsi.apps.googleusercontent.com";
